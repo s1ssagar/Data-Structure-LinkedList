d7cf15dd0f2c91579fea138b63d60dd1f39a7894
s1ssagar <s1ssagar.s1ssingh@gmail.com>
2017-12-31 19:37:53 +0530

    Added non recursive method for adding nodes. Comparison between in operator python and search in binary search tree.

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -1,4 +1,5 @@
-from datetime import datetime
+import time
+import random
 class Node:
     def __init__(self,data):
         self.data = data
@@ -31,6 +32,30 @@ class bst:
                 else:
                     self.add_node(node.right, data)
     
+    def add_no_recur(self,data):
+        self.add_node_no_recur(self.btree, data)
+    
+    def add_node_no_recur(self, node, data):
+        if self.btree == None:
+            node_ = Node(data)
+            self.btree = node_
+        else:
+            while 1:
+                if data <= node.data:
+                    if node.left == None:
+                        temp_node = Node(data)
+                        node.left = temp_node
+                        break
+                    else:
+                        node = node.left
+                else:
+                    if node.right == None:
+                        temp_node = Node(data)
+                        node.right = temp_node
+                        break
+                    else:
+                        node = node.right
+    
     def inorder(self):
         return self.print_inorder(self.btree)
     
@@ -43,21 +68,34 @@ class bst:
         return self.arr
 
     def search_data(self,node,data):
+        i = 0
+        start_time = time.time()
         while node != None:
             if node.data == data:
+                print i,"\niterations\n"
+                print '{:.6f}'.format((time.time() - start_time))
                 return True
             elif data > node.data:
                 node = node.right
             else:
                 node = node.left
+            i += 1
         return False
     
     def search(self,data):
         return self.search_data(self.btree,data)
 
 b_tree = bst()
-for x in range(100,0,-1):
-    b_tree.add(x)
-time = datetime.now()
+arr = []
+print "\nMaking array\n"
+while len(arr[:]) < 10000:
+    num = random.randint(1, 10000)
+    if num not in arr:
+        arr.append(num)
+for x in arr:
+    b_tree.add_no_recur(x)
 print b_tree.search(25)
-print datetime.now() - time
+start_time = time.time()
+if 25 in arr:
+    print arr.index(25)
+print '{:.6f}'.format((time.time() - start_time))
